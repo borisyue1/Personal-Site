fe3a350a9af02aa60ad98eb7f8d0c153331e332d
Boris Yue <borisyue@yahoo.com>
2016-12-26 15:02:38 -0800

    finished title page

--- app/js/main.js
+++
@@ -1,9 +0,0 @@
-//vertically align
-$(document).ready(function(){
-  var height = $('#header').height();
-  var h = height/2;
-  var w1 = $(window).height();
-  var w = w1/2;
-  var m = w - h
-  $('#header').css("margin-top",m - 50 + "px")
-});

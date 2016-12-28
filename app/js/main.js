eedfcf9e618164c35de687346d20801b582e5854
Boris Yue <borisyue@yahoo.com>
2016-12-28 03:20:00 -0800

    finished navbar highlighting and added two lines

---
+++ app/js/main.js
@@ -0,0 +1,34 @@
+$('a[href^="#"]').on('click', function(event) {//smooth scrolling
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 25
+        }, 1000);
+    }
+});
+
+//for scrolling navbar
+// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+var mainbottom = $('#home').height();
+// on scroll, 
+$(window).on('scroll',function(){
+    var stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom - 100) {
+        $('.nav-bar').addClass('past-home');
+    } else {
+        $('.nav-bar').removeClass('past-home');
+    }
+    checkPosition(stop, mainbottom);
+});
+function checkPosition(stop, mainbottom){
+    $('li a').removeClass('active');
+    if(stop < mainbottom - 100){
+        $('#home-tag').addClass('active');
+    }
+    else if(stop > mainbottom - 100){
+        $('#about-tag').addClass('active');
+    } 
+
+}
+
